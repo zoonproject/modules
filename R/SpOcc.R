5387921db181ad28e434dee1541178a591a41573
tim lucas <timcdlucas@gmail.com>
2014-08-14 11:36:18 +0100

    Spocc module added

---
+++ R/SpOcc.R
@@ -0,0 +1,11 @@
+# A zoon module
+# @occurrence
+SpOcc <-
+function(species, extent, databases = c('gbif', 'bison', 'inat', 'ebird', 'ecoengine')){
+  require(spocc)
+  raw <- occ2df(occ(query = species, geometry = extent, from = databases, limit=10e5))
+  occurrence <- raw[,c('longitude', 'latitude')]
+  occurrence$value <- 1
+  occurrence$type <- 'presence'
+  return(occurrence) 
+}
