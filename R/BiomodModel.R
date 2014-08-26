82e7a996706f785a3609fc7206bf2ac8c9023fa5
tim lucas <timcdlucas@gmail.com>
2014-08-26 19:17:23 +0100

    Trying to get envir right. removed prints

--- R/BiomodModel.R
+++ R/BiomodModel.R
@@ -72,10 +72,6 @@ function(df, modelType){
 
   assign('predict.BIOMOD.models.out', biomodPredictMethod, pos = sys.frame(-2))
 
-  print('HEllo')
-  print(where('biomodPredictMethod'))
-  print(where('predict.BIOMOD.models.out'))
-
   return(myBiomodModelOut)
 
 }
