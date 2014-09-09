2bc1bc4ff4b6563bf3ee0f14375fef7ae44b791c
tim lucas <timcdlucas@gmail.com>
2014-09-09 16:08:45 +0100

    Files actually removed this time

--- R/UKAnophelesPlumbeus.R
+++
@@ -1,8 +0,0 @@
-#'Return some Anopheles plumbeus data that is bundled with package.
-#'  Data taken from GBIF and is UK extent.
-#'
-#'@name UKAnophelesPlumbeus
-UKAnophelesPlumbeus <-
-function(){
-  return(AplumbeusOcc)
-}
