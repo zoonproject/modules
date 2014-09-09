38b9cd285234a736c7f470401358094328d29fe9
tim lucas <timcdlucas@gmail.com>
2014-09-09 14:48:54 +0100

    surfacemap and print map files added

---
+++ R/SurfaceMap.R
@@ -0,0 +1,28 @@
+#'Output module: SurfaceMap
+#'
+#'Make a png of a map of predicted surface.
+#'
+#'@param dir Where to save figures. Defaults to the working directory. 
+
+#'@name SurfaceMap
+SurfaceMap <-
+function (model, ras, dir='.') {
+  
+  vals <- data.frame(getValues(ras))
+  colnames(vals) <- names(ras)
+  
+  pred <- predict(model,
+                  newdata = vals,
+                  type = 'response')
+  
+  pred_ras <- ras[[1]]
+  
+  pred_ras <- setValues(pred_ras, pred)
+
+  png(paste0(dir,"/ZoonMap", Sys.time(), ".png"))
+  plot(ras) 
+  dev.off()
+  
+  return(NULL)
+  
+}
