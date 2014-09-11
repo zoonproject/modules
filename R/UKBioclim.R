af89a7930ebba7e65387b3c0feec04a2edec6ec4
Nick <goldingn35@gmail.com>
2014-09-11 13:23:07 +0100

    added UK bioclim covariate function, which should look much nicer than the NCEP air temperature one at this scale

---
+++ R/UKBioclim.R
@@ -0,0 +1,15 @@
+#'Covariate module: UKBioclim
+#' Load Bioclim rasters at 5 degree resolution for the UK
+#' (a region bounded by longitudes -12 and 4 and by latitudes 50 and 60).
+#' The first time this module is used the data are downloaded from the web,
+#' subsequently a local copy is used so it's much faster.
+#' This is essentially a wrapper around the `raster` function `getData`,
+#' see `?getData' for more details of what that function does.
+
+#'@name UKBioclim
+UKBioclim <-
+  function() {
+    world <- getData('worldclim', var = 'bio', res = 5)
+    uk <- crop(tmp, extent(-12, 4, 50, 60))
+    return (uk)
+  }
