e730b04b2b337d1fbe40bd52ca8d631fa75f0df7
tim lucas <timcdlucas@gmail.com>
2014-09-12 12:36:18 +0100

    external vlaid added in localoccurrencedata.r

---
+++ R/LocalOccurrenceData.R
@@ -0,0 +1,35 @@
+#'Occurrence module: LocalOccurrenceData
+#'
+#'Occurrence module to format local occurrence data to be used with zoon.
+#'
+#'@param filename The path to the file. Currently assumes a .csv file. 
+#'
+#'@param occurrenceType What type data is it? One of 'presence', 'presence/absence', 'abundance' 
+#'
+#'@param externalValidation Logical indicating whether this data is external validation data. Cross validation is handled elsewhere and this argument should be left as FALSE for data that will be used in cross validation. 
+#'
+#'@name LocalOccurrenceData
+LocalOccurrenceData <-
+function(filename, occurrenceType, externalValidation=FALSE){
+
+  assert_that(is.logical(externalValidation))
+  type <- tolower(occurrenceType)
+  #assert_that(type %in% c('presence', 'presence/absence', 'abundance'))
+  occurrence <- read.csv(filename, header=TRUE)
+  
+  if(type == 'presence/absence') {
+    occurrence$type <- ifelse(occurrence[,3]==1, 'presence', 'absence')
+  } else {
+    occurrence$type <- type
+  }
+
+  colnames(occurrence) <- c('lon', 'lat', 'value', 'type')
+
+  if (!externalValidation){
+    occurrence$fold <- 0
+  } else {
+    occurrence$fold <- 1
+  }                     
+
+  return(occurrence)
+}
