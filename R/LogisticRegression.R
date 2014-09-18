c5c8121eb197ca3961537d5b588399d125783084
tim lucas <timcdlucas@gmail.com>
2014-09-18 12:57:21 +0100

    Removed checks from logreg and randfor for now

--- R/LogisticRegression.R
+++ R/LogisticRegression.R
@@ -8,9 +8,9 @@
 
 
 LogisticRegression <- function(df){
-  if (!all(df$type %in% c('presence', 'absence', 'background'))) {
-    stop ('only for presence/absence or presence/background data')
-  }
+  #if (!all(df$type %in% c('presence', 'absence', 'background'))) {
+  #  stop ('only for presence/absence or presence/background data')
+  #}
   
   covs <- as.data.frame(df[, 6:ncol(df)])
   names(covs) <- names(df)[6:ncol(df)]
