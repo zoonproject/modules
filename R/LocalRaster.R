36de431d709c0266c54a88d1eba9c64da2e4677c
timcdlucas <timcdlucas@gmail.com>
2014-10-25 00:40:45 +0100

    Final modules commit

--- R/LocalRaster.R
+++ R/LocalRaster.R
@@ -1,10 +1,15 @@
-#'Covariate module: LocalRaster
+#'LocalRaster: Read in a single raster or a list of rasters and stack them.
 #'
 #'Read in a single raster or a list of rasters and stack them.
+#'
+#'Module Type: Covariate
+#'
 #'
 #'@param filenames Either a string of the filename of the raster layer, or a list of strings of filenames to rasters that will be stacked. 
 #'
 #'@name LocalRaster
+#'@author Tim Lucas
+#'@author Email: timcdlucas@@gmail.com
 LocalRaster <-
 function(filenames){
 
