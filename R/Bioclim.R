d9019e5830fdbf7aa6224c681bca2a529754dd40
Nick Golding <goldingn35@gmail.com>
2015-09-14 21:13:21 +0100

    adding layers argument to Bioclim

--- R/Bioclim.R
+++ R/Bioclim.R
@@ -6,20 +6,29 @@
 #'
 #'@param resolution Resolution in minutes. Must be one of 2.5, 5 or 10. Default is 5. 
 #'
+#'@param layers which bioclim layers to obtain, a vector of integers between 1 and 19.
+#'
 #'@name Bioclim
 #'@family covariate
 Bioclim <-
-function(extent, resolution = 5) {
+function(extent, resolution = 5, layers = 1:19) {
     
     if(!(resolution %in% c(2.5, 5, 10))){
       stop('only 2.5, 5 and 10 degree resolutions are supported currently')
     }
+  
+  if (length(layers) < 1 |
+      length(layers) > 19 |
+      !all(layers %in% 1:19)) {
+    stop ('layers must be a vector of integers between 1 and 19 indicating layers to obtain')
+  }
 
     stopifnot(length(extent) == 4)
     stopifnot(all(is.numeric(extent)))
 
 
     world <- getData('worldclim', var = 'bio', res = resolution)
+    world <- world[[layers]]
     cropped <- crop(world, extent(extent))
     return (cropped)
   }
