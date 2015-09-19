21962717e6c56b20274ed7757d19991f1f7df72c
Tim Lucas <timcdlucas@gmail.com>
2015-09-19 11:22:21 +0100

    Bug in PrintMap fixed. Closes zoonproject/zoon#172. Referred to `model` instead of `.model`. Doing stuff interactively affects what is happening in `workflow` which can be dangerous.

--- R/PrintMap.R
+++ R/PrintMap.R
@@ -25,11 +25,14 @@ function (.model, .ras) {
   par(mar = c(4, 4, 0, 2) + 0.1)
   plot(pred_ras, col = cls, xlab = 'Longitude', ylab = 'Latitude') 
 
-
-  points(.model$df$longitude[model$df$value == 0], .model$df$latitude[model$df$value == 0], 
+  if(any(.model$df$value == 0)){
+    points(.model$df$longitude[.model$df$value == 0], .model$df$latitude[.model$df$value == 0], 
     pch = 16, col = '#00000055')
-  points(.model$df$longitude[model$df$value == 1], .model$df$latitude[model$df$value == 1], 
+  }
+  if(any(.model$df$value == 1)){
+    points(.model$df$longitude[.model$df$value == 1], .model$df$latitude[.model$df$value == 1], 
     pch = 16, col = '#e41a1c55')
+  }
   legend('topright', legend = c('Presence', 'Absence'), fill = c( '#e41a1c', '#000000'), 
     bty = 'n', border = NA, inset=c(-0.23, 0.1), xpd = TRUE)
 
