5820d837999deed1776d2d6a5216477d9aa6bbe7
AugustT <tomaug@ceh.ac.uk>
2015-10-29 11:34:46 +0000

    move module testing to local files and simplify into one function

--- tests/testthat/test-genericTests.R
+++ tests/testthat/test-genericTests.R
@@ -1,33 +1,29 @@
 ### General module tests ###
 context('General module tests')
 
-if (!capabilities('libcurl')) skip('skipping as libcurl not supported')  
-
 library(zoon, quietly = TRUE)
-
-# These tests are generic and do not depend on module type
-modules <- unlist(GetModuleList())
-
-baseURL <- 'https://raw.githubusercontent.com/zoonproject/modules/master/R/'
-
 library(roxygen2)
-library(RCurl)
 
-# loop through each module
-for(moduleURL in paste(baseURL, modules, '.R', sep = '')){
+# Get our list of modules
+modulePaths <- list.files('../../R', pattern = '.R$', full.names = TRUE)
+
+# A vector of R scripts to ignore
+ignoreModules <- c('ModulesDocumentation.R')
+modulePaths <- modulePaths[!basename(modulePaths) %in% ignoreModules]
+
+test_module <- function(modulePath){
   
-  test_that(paste('Check urls exist -', basename(gsub('.R', '', moduleURL))),{
+  roxy_parse <- roxygen2:::parse_file(modulePath, environment())[[1]]
+  
+  ## GENERIC TESTS
+  test_that(paste('Check roxy_parse', basename(gsub('.R', '', modulePath))),{
     
-    # expect that the URL exists
-    expect_true(url.exists(moduleURL))
-    closeAllConnections()
+    # Check roxy_parse is good
+    expect_is(roxy_parse, 'list')
     
   })
   
-  test_that(paste('Check for generic tags', basename(gsub('.R', '', moduleURL))),{
-    
-    roxy_parse <- roxygen2:::parse_file(moduleURL, environment())[[1]]
-    closeAllConnections()
+  test_that(paste('Check for generic tags', basename(gsub('.R', '', modulePath))),{
     
     # Check for the required generic tags
     expect_true('title' %in% names(roxy_parse))
@@ -37,5 +33,71 @@ for(moduleURL in paste(baseURL, modules, '.R', sep = '')){
     expect_true('author' %in% names(roxy_parse))
     
   })
+  
+  # If the family tag is present continue (this error will be reported above)
+  if('family' %in% names(roxy_parse)){
+    
+    ## OCCURRENCE TESTS
+    if(roxy_parse$family == 'occurrence'){
+      
+      test_that(paste('Check occurrence module -', basename(gsub('.R', '', modulePath))), {
+        
+        expect_equal(roxy_parse$family, 'occurrence')
+        
+      })
+      
+    }
+    
+    ## COVARIATE TESTS
+    if(roxy_parse$family == 'covariate'){
+      
+      test_that(paste('Check covariate module -', basename(gsub('.R', '', modulePath))), {
+        
+        expect_equal(roxy_parse$family, 'covariate')
+        
+      })
+      
+    }  
+    
+    ## PROCESS TESTS
+    if(roxy_parse$family == 'process'){
+      
+      test_that(paste('Check process module -', basename(gsub('.R', '', modulePath))), {
+        
+        expect_equal(roxy_parse$family, 'process')
+        
+      })
+      
+    }  
+    
+    ## MODEL TESTS
+    if(roxy_parse$family == 'model'){
+      
+      test_that(paste('Check model module -', basename(gsub('.R', '', modulePath))), {
+        
+        expect_equal(roxy_parse$family, 'model')
+        
+      })
+      
+    }
+    
+    ## OUTPUT TESTS
+    if(roxy_parse$family == 'output'){
+      
+      test_that(paste('Check output module -', basename(gsub('.R', '', modulePath))), {
+        
+        expect_equal(roxy_parse$family, 'output')
+        
+      })
+      
+    }
+  }
+  
+}
+
+# loop through each module
+for(modulePath in modulePaths){
+  
+  test_module(modulePath) 
   
 }
