232b364761c107ea2e9d90134b139bb18ee78796
AugustT <tomaug@ceh.ac.uk>
2016-02-09 10:23:12 +0000

    Data type added to MaxEnt and Random forest

--- R/MaxEnt.R
+++ R/MaxEnt.R
@@ -26,6 +26,7 @@
 #' @author ZOON Developers, \email{zoonproject@@gmail.com}
 #' @section Version: 1.0
 #' @section Date submitted: 2015-11-13
+#' @section Data type: presence/background
 #'
 #' @name MaxEnt
 #' @family model
