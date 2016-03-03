5bf240f440c0f2d1077875f182eac18bb69b8fe8
Samuel <samuel.bosch@gmail.com>
2016-03-03 17:00:38 +0100

    add support for extra input types
    Added support for vectors of filenames, RasterLayer and RasterStack objects

--- R/LocalRaster.R
+++ R/LocalRaster.R
@@ -4,22 +4,27 @@
 #'
 #' @details Read in a single raster or a list of rasters and stack them.
 #'
-#' @param filenames Either a string of the filename of the raster layer, or a list of strings of filenames to rasters that will be stacked. 
+#' @param rasters Either a string of the filename of the raster layer, a list or vector of strings of filenames to rasters that will be stacked, a RasterLayer or a RasterStack object. 
 #'
 #' @name LocalRaster
 #' @family covariate
-#' @author Tim Lucas \email{timcdlucas@@gmail.com}
-#' @section Version: 1.0
-#' @section Date submitted: 2015-11-13
+#' @author Tim Lucas \email{timcdlucas@@gmail.com} and Samuel Bosch \email{mail@samuelbosch.com}
+#' @section Version: 1.1
+#' @section Date submitted: 2016-03-03
 LocalRaster <-
-function(filenames='myRaster'){
+function(rasters='myRaster'){
 
-  if(is.string(filenames)){
-    raster <- raster(filenames)
-  } else if(is.list(filenames)) {
-    rasterList <- lapply(filenames, raster)
+  if(is.string(rasters)){
+    raster <- raster(rasters)
+  } else if(is.list(rasters)) {
+    rasterList <- lapply(rasters, raster)
     raster <- stack(rasterList)
+  } else if(is.vector(rasters) & typeof(rasters) == "character") {
+    raster <- stack(rasters)
+  } else if (is(rasters, "RasterLayer") || is(rasters, "RasterStack")) {
+    raster <- rasters
+  } else {
+    stop("LocalRaster: rasters type is not supported (currently supported types: string, list of strings, vector of strings, RasterLayer and RasterStack)")
   }
-
   return(raster)
 }
