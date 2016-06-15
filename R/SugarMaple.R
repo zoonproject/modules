67cb072c4cbabbb58949d4f3a8b5b7176284b374
Geoff Williams <geoff.williams@cs.ox.ac.uk>
2016-06-15 10:50:12 +0000

    Upload from ZOON web interface

--- R/SugarMaple.R
+++ R/SugarMaple.R
@@ -4,7 +4,7 @@
 #'
 #' @description Presence/absence of Sugar Maple (\emph{Acer saccharum}) in North America. Each record corresponds to the centroid of a 0.5 degree cell.
 #'
-#' @details The extent of the occurrence data is \\code{c(-170, -20, 10, 80)}. These data were used as part of a wider analysis by Morin & Thuiller (2009), and were subsequently analysed and made available by Talluto \emph{et al.}(2016). \n Morin, X., & Thuiller, W. (2009). Comparing niche-and process-based models to reduce prediction uncertainty in species range shifts under climate change. Ecology, 90(5), 1301–1313. http://doi.org/10.1890/08-0134.1 \n Talluto, M. V., Boulangeat, I., Ameztegui, A., Aubin, I., Berteaux, D., Butler, A., … Gravel, D. (2016). Cross-scale integration of knowledge for predicting species ranges: A metamodelling framework. Global Ecology and Biogeography, 25(2), 238–249. http://doi.org/10.1111/geb.12395
+#' @details The extent of the occurrence data is \code{c(-170, -20, 10, 80)}. These data were used as part of a wider analysis by Morin & Thuiller (2009), and were subsequently analysed and made available by Talluto \emph{et al.}(2016). \n Morin, X., & Thuiller, W. (2009). Comparing niche-and process-based models to reduce prediction uncertainty in species range shifts under climate change. Ecology, 90(5), 1301–1313. http://doi.org/10.1890/08-0134.1 \n Talluto, M. V., Boulangeat, I., Ameztegui, A., Aubin, I., Berteaux, D., Butler, A., … Gravel, D. (2016). Cross-scale integration of knowledge for predicting species ranges: A metamodelling framework. Global Ecology and Biogeography, 25(2), 238–249. http://doi.org/10.1111/geb.12395
 #'
 #' @family occurrence
 #'
@@ -12,7 +12,7 @@
 #'
 #' @section Data type: presence/absence
 #'
-#' @section Version: 0.1
+#' @section Version: 0.2
 #'
 #' @section Date submitted:  2016-06-15
 SugarMaple <- function () 
