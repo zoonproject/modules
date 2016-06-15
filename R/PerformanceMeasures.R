cec2c2f4b52e0a0968dd1f38bd17756bc5885b0e
Geoff Williams <geoff.williams@cs.ox.ac.uk>
2016-06-15 11:59:28 +0000

    Upload from ZOON web interface

--- R/PerformanceMeasures.R
+++ R/PerformanceMeasures.R
@@ -17,30 +17,37 @@
 #' @param .ras \strong{Internal parameter, do not use in the workflow function}. \code{.ras} is a raster layer, brick or stack object. \code{.ras} is passed automatically in workflow from the covariate module(s) to the output module(s) and should not be passed by the user.
 #'
 #' @param threshold A chosen threshold value for measures that need 0/1 predictions 
+#'   If NULL, the proportion of occurrences in the occurrence data is used.
 #'
 #' @author ZOON Developers, \email{zoonproject@@gmail.com}
 #' @section Version: 1.0
-#' @section Date submitted: 2015-11-13
+#' @section Date submitted: 2016-06-15
+#' @section Data type: presence/absence, presence/background
 #'
 #' @name PerformanceMeasures
 #' @family output
 PerformanceMeasures <-
-function(.model, .ras, threshold = 0.5){
+function(.model, .ras, threshold = NULL){
 
   zoon:::GetPackage('SDMTools')
+
+  if(is.null(threshold)){
+    # If threshold is not given, use the proportion of the data set that is presence.
+    threshold <- mean(.model$data$value)
+  }
 
   if (all(.model$data$predictions %in% c(0,1))){
     warning('The model has predicted presence/absence rather than probabilities. Some measures may not work')
   }
 
   if (all(.model$data$fold == 1)){
+
     warning('You have no cross-validation folds, validation statistics may be misleading')
 
     # make predictions for the model
     covs <- .model$data[, 7:NCOL(.model$data), drop = FALSE]
     p <- ZoonPredict(zoonModel = .model$model,
                      newdata = covs)
-    
     confusion <- SDMTools::confusion.matrix(.model$data$value,
                                             p,
                                             threshold)
@@ -81,14 +88,14 @@ function(.model, .ras, threshold = 0.5){
     )
   }
   
-  message('Model performance measures:')
-  for(i in 1:length(performance)) {
-    line <- paste0(names(performance)[i],
-                   ' :  ',
-                   performance[i])
-    message(line)
-  }
-  message(' ')
+#  message('Model performance measures:')
+#  for(i in 1:length(performance)) {
+#    line <- paste0(names(performance)[i],
+#                   ' :  ',
+#                   performance[i])
+#    message(line)
+#  }
+#  message(' ')
 
   return (performance)
 
