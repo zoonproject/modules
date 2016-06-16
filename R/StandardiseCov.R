56e43d252f1603fee384192b41c3a2bb9800afbd
Geoff Williams <geoff.williams@cs.ox.ac.uk>
2016-06-16 13:42:45 +0000

    Upload from ZOON web interface

---
+++ R/StandardiseCov.R
@@ -0,0 +1,48 @@
+#' @name StandardiseCov
+#'
+#' @title Scale numeric covariates
+#'
+#' @description The module by default scales all numeric covariates. By default conducts a regular standardisation: minus the mean and divide by the standard deviation. If Gelman = TRUE then divides by 2 sd as suggested by Gelman. Excluded covariates are not standardised.
+#'
+#' @details 
+#'
+#' @param .data \strong{Internal parameter, do not use in the workflow function}. \code{.data} is a list of a data frame and a raster object returned from occurrence modules and covariate modules respectively. \code{.data} is passed automatically in workflow from the occurrence and covariate modules to the process module(s) and should not be passed by the user.
+#'
+#' @param Gelman Option to divide by two rather than one standard deviation.
+#'
+#' @param exclude Variable names of numeric variables to exclude from standardisation
+#'
+#' @family process
+#'
+#' @author Alison Johnston & Carsten F. Dormann, \email{alison.johnston@@bto.org}
+#'
+#' @section Data type: presence-only, presence/absence, abundance, proportion
+#'
+#' @section Version: 0
+#'
+#' @section Date submitted:  2016-06-16
+StandardiseCov <- function (.data, Gelman = FALSE, exclude = NULL) 
+{
+    df <- .data$df
+    ras <- .data$ras
+    numericLayer <- NA
+    for (i in 1:nlayers(ras)) numericLayer[i] <- is.numeric(ras[i]) & 
+        !names(ras)[i] %in% exclude
+    if (sum(numericLayer) == 0) 
+        stop("StandardiseCov not used, as no numeric covariates")
+    numericNames <- names(ras)[which(as.logical(numericLayer))]
+    sd_mult <- ifelse(Gelman, 2, 1)
+    for (cov in numericNames) {
+        m <- cellStats(ras[[cov]], "mean", na.rm = TRUE)
+        s <- cellStats(ras[[cov]], "sd", na.rm = TRUE) * sd_mult
+        if (nlayers(ras) == 1) {
+            ras <- (ras - m)/s
+        }
+        else {
+            ras[[cov]] <- (ras[[cov]] - m)/s
+        }
+    }
+    layer <- extract(ras, df[, c("longitude", "latitude")])
+    df <- cbind(df[, 1:5], layer)
+    return(list(df = df, ras = ras))
+}
