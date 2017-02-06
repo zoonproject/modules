433253668c343525ca9068605efe4c56540ca17c
Dr Tom August <tomaug@ceh.ac.uk>
2017-02-06 11:27:32 +0000

    Fix ResponseCurveViz
    The '{' needs to be on the same line as the function definition

--- R/ResponseCurveViz.R
+++ R/ResponseCurveViz.R
@@ -21,8 +21,7 @@
 #' @section Version: 0
 #'
 #' @section Date submitted:  2016-06-15
-ResponseCurveViz <- function (.model, .ras, cov = NULL) 
-{
+ResponseCurveViz <- function (.model, .ras, cov = NULL) {
     def.par <- par(no.readonly = TRUE)
     params <- colnames(.model$data)[-(1:6)]
     nparams <- length(params)
