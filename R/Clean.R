e29b33969711fe6f56bea7c9cefeb582e0e8c491
David Wilkinson <davidpw@student.unimelb.edu.au>
2017-08-09 11:24:37 +1000

    Updated Clean module to print to console the number of data points removed

--- R/Clean.R
+++ R/Clean.R
@@ -15,57 +15,103 @@
 #'  \item{"3 - Unlikely"}{Remove rows with unlikely data such as (0, 0)}
 #' }
 #' 
-#' @author ZOON Developers, \email{zoonproject@@gmail.com}
-#' @section Version: 1.0
+#' @author ZOON Developers, David Wilkinson \email{zoonproject@@gmail.com}
+#' @section Version: 1.1
 #' @section Date submitted: 2015-03-21
+#' @section Date updated: 2017-08-09
 #' @section Data type: presence-only, presence/absence, abundance, proportion
 #'
 #' @name Clean
 #' @family process
 
 
-Clean <-
-function (.data, which = c(1, 2, 3, 4)) {
-
+Clean <- function (.data, which = c(1, 2, 3, 4)) {
+  
   # Make lower case clean argument to make comparisons easier  
   if(is.character(which)) which <- tolower(which)
-
+  
   # The list of possible entries in clean (as a character vector).
   listOfPossible <- c('impossible', 'incomplete', 'unlikely')
-
+  
   # If any items in clean aren't available, throw an error.
   if(!all(which %in% 1:4)){
     if(!all(which %in% listOfPossible)){
       stop('In Clean module you have selected cleaning processes that do not exist. Please alter "which" argument')
     }
   }
-
+  
+  # Extract point data
+  
   occurrence <- .data$df
+  
+  # Number of data points pre-Clean
+  
+  numPoints_start <- nrow(occurrence)
+  numPointsPres_start <- nrow(occurrence[type == "presence", ])
+  numPointsAbs_start <- nrow(occurrence[type == "absence", ])
+  numPointsBack_start <- nrow(occurrence[type == "background", ])
   
   # Load the scrubr package for data cleaning
   zoon::GetPackage('scrubr')  
-
+  
   # Remove impossible lat lon rows.
   if (1 %in% which | 'impossible' %in% which){
     occurrence <- coord_impossible(occurrence, lat = 'latitude', lon = 'longitude')
   }
-
+  
   # remove incomplete data rows
   if (2 %in% which | 'incomplete' %in% which){
     occurrence <- coord_incomplete(occurrence, lat = 'latitude', lon = 'longitude')
   }
-
+  
   # Remove unlikely datapoints
   if (3 %in% which | 'unlikely' %in% which){
     occurrence <- coord_unlikely(occurrence, lat = 'latitude', lon = 'longitude')
   }
-
-#  # Remove data points close to city (and maybe country?) centroids
-#  if (4 %in% clean | 'pol_centroids' %in% clean){
-#    occurrence <- coord_pol_centroids(occurrence)
-#  }
-
-
+  
+  #  # Remove data points close to city (and maybe country?) centroids
+  #  if (4 %in% clean | 'pol_centroids' %in% clean){
+  #    occurrence <- coord_pol_centroids(occurrence)
+  #  }
+  
+  # Number of data points post-Clean
+  
+  numPoints_end <- nrow(occurrence)
+  numPointsPres_end <- nrow(occurrence[type == "presence", ])
+  numPointsAbs_end <- nrow(occurrence[type == "absence", ])
+  numPointsBack_end <- nrow(occurrence[type == "background", ]) 
+  
+  # Number of data points removed
+  
+  points_removed <- numPoints_start - numPoints_end
+  pointsPres_removed <- numPointsPres_start - numPointsPres_end
+  pointsAbs_removed <- numPointsAbs_start - numPointsAbs_end
+  pointsBack_removed <- numPointsBack_start - numPointsBack_end
+  
+  if(is.null(pointsPres_removed)){
+    pointsPres_removed <- 0
+  }
+  if(is.null(pointsAbs_removed)){
+    pointsAbs_removed <- 0
+  }
+  if(is.null(pointsBack_removed)){
+    pointsBack_removed <- 0
+  }
+  
+  # Generate message to print to screen
+  
+  points_rem_message <- paste(
+    "The Clean module has removed", points_removed, "data points.",
+    "This includes", pointsPres_removed, "presences,", pointsAbs_removed,
+    "absences, and", pointsBack_removed, "background points.",
+    collapse = " ")
+  
+  # print message to screen
+  
+  message(points_rem_message)
+  
+  # return object
+  
   return(list(df = occurrence, ras = .data$ras))
   
 }
