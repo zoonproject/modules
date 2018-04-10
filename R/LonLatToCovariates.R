2ad727e86a11ede0a5b592aff5de16af37937b7a
Tim Lucas <timcdlucas@gmail.com>
2018-04-10 22:58:07 +0100

    Make lonlat columns optional. Might want just latitude for example.

--- R/LonLatToCovariates.R
+++ R/LonLatToCovariates.R
@@ -2,10 +2,12 @@
 #'
 #' @title Create new covariates using latitude and longitude values
 #'
-#' @description Create two new covariates, Longitudecov and Latitudecov that are simply the latitude and longitude for each cell. These covariates can be used in generally non-spatial models. Some specifically spatial models such as INLA require these covariates and will use the specifically named covariates.
+#' @description Create two new covariates, Longitudecov and Latitudecov that are simply the latitude and longitude for each cell. These covariates can be used in otherwise non-spatial models. Some specifically spatial models such as INLA require these covariates and will use the specifically named covariates once implemented.
 #'
 #' @details 
 #'
+#' @param lat Logical, return the latitude column?
+#' @param lon Logical, return the longitude column?
 #' @param .data \strong{Internal parameter, do not use in the workflow function.} \code{.data} is a list of a data frame and a raster object returned from occurrence modules and covariate modules respectively. .data is passed automatically in workflow from the occurrence and covariate modules to the process module(s) and should not be passed by the user.
 #'
 #' @family process
@@ -17,11 +19,13 @@
 #' @section Version: 0.1
 #'
 #' @section Date submitted:  2017-04-25
-LonLatToCovariates <- function (.data) {
+LonLatToCovariates <- function (.data, lat = TRUE, lon = TRUE) {
     
     df <- .data$df
     ras <- .data$ras
     
+    if(!lat & !lon) stop('You must chose to return at least one of lat and lon.')
+
     # create copy of rasters
     lonlat <- raster::stack(ras[[1]], ras[[1]])
     
