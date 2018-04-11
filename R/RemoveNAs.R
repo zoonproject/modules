11eb2301ecdcf522f5fb9e88278ec966b8593ef1
Tim Lucas <timcdlucas@gmail.com>
2018-04-11 19:40:42 +0100

    Tidy up removeNAs.

--- R/RemoveNAs.R
+++ R/RemoveNAs.R
@@ -7,33 +7,34 @@
 #'
 #' @param .data \strong{Internal parameter, do not use in the workflow function}. \code{.data} is a list of a data frame and a raster object returned from occurrence modules and covariate modules respectively. \code{.data} is passed automatically in workflow from the occurrence and covariate modules to the process module(s) and should not be passed by the user.
 #'
+#' @param responseOnly Logical indicating whether to only remove rows with NAs in the response column.
 #' @param ignore Character vector of columns to ignore i.e. columns that can have NAs without being removed. 
 #'
 #'
 #' @author ZOON Developers, \email{zoonproject@@gmail.com}
 #' @section Version: 1.0
-#' @section Date submitted: 2018-02-25
+#' @section Date submitted: 2018-04-11
 #'
 #' @section Data type: presence-only, presence/absence, abundance, proportion
 #'
-#' @name NoProcess
+#' @name RemoveNAs
 #' @family process
 
 RemoveNAs <-
   function (.data, responseOnly = FALSE, ignore = NULL) {
     
+    stopifnot(is.logical(responseOnly))
+
     df <- .data$df
     
     if(responseOnly){
       df <- df[!is.na(df$value), ]
     } else {
-      df <- df[complete.cases(df), ]
+      df <- df[complete.cases(df[, !names(df) %in% ignore]), ]
     }
     
     
-    ras <- .data$ras
-    
-    return(list(df=df, ras=ras))
+    return(list(df=df, ras=.data$ras))
     
   }
 
